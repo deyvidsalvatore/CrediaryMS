d4b3b9ded39a1e7192b3c1afba8c9d5791133c6d
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2025-01-15 10:53:22 -0300

    initial eureka server

---
+++ eurekaserver/src/test/java/com/deyvidsalvatore/crediary/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deyvidsalvatore.crediary.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
