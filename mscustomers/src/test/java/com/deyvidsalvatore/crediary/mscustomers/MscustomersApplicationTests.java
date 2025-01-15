253558aa13af77ad82278b99f9b102e358f02146
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2025-01-15 12:12:58 -0300

    initial and config mscustomers

---
+++ mscustomers/src/test/java/com/deyvidsalvatore/crediary/mscustomers/MscustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deyvidsalvatore.crediary.mscustomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
