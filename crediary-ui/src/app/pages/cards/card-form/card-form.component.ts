fdfed25dc445b21d8d93e1e9dc55aa8a19fdafc2
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2025-01-17 09:23:29 -0300

    feat: Card Form

--- crediary-ui/src/app/pages/cards/card-form/card-form.component.ts
+++ crediary-ui/src/app/pages/cards/card-form/card-form.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { CardBrand } from '../../../shared/enums/CardBrand';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-card-form',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './card-form.component.html',
   styleUrl: './card-form.component.scss'
 })
-export class CardFormComponent {
+export class CardFormComponent implements OnInit, OnDestroy {
 
+  cardForm = new FormGroup<any>({});
+  cardBrands = Object.values(CardBrand);
+  private fb = inject(FormBuilder);
+
+  private destroy$ = new Subject<void>();
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  initializeForm(): void {
+    this.cardForm = this.fb.group({
+      name: ['', Validators.required],
+      brand: ['', Validators.required],
+      income: [0, Validators.required],
+      basicLimit: [0, Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.cardForm.getRawValue());
+  }
 }
